c7f207512daccf9ee7937ab21984e5284e07d45a
amir <amirdehghani137722@gmail.com>
2024-03-12 10:31:34 +0330

    add subCategories

--- src/components/FilterBox/index.tsx
+++ src/components/FilterBox/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import React, { useState } from "react"
 import Nodes from "../../api/Nodes"
 import { useConstructor } from "../../help"
@@ -10,10 +11,12 @@ interface FilterBox {
 const FilterBox:React.FC<FilterBox> = ({exeNods,setexeNods}) => {
     const [categories,setCategories] = useState<Array<string>>([])
     const [checkAll,setCheckAll] = useState(true)
+    const [AllCategories,setAllCategories] = useState<any>([])
     useConstructor(() => {
         const nodeApi = new Nodes()
         nodeApi.getCatagories((res) => {
             // console.log(Object.entries(res).map(item => item[0]))
+            setAllCategories(res)
             setCategories(Object.entries(res).map(item => item[0]))
         })
     })
@@ -27,7 +30,14 @@ const FilterBox:React.FC<FilterBox> = ({exeNods,setexeNods}) => {
                         <input type="checkbox" value={'all'} checked={checkAll} onChange={() => {
                             setCheckAll(!checkAll)
                             if(checkAll){
-                                setexeNods(categories)
+                                const allData=categories
+                                categories.forEach((el) => {
+                                    const child:Array<string> = AllCategories[el]
+                                    child?.forEach((el2) => {
+                                        allData.push(el2)
+                                    })
+                                })
+                                setexeNods(allData)
                             }else{
                                 setexeNods([])
                             }
@@ -36,16 +46,39 @@ const FilterBox:React.FC<FilterBox> = ({exeNods,setexeNods}) => {
                     </div>                    
                     {categories.map((item:string) => {
                         return (
-                            <div className="flex items-start mb-2 justify-start">
-                                <input type="checkbox" value={item} checked={exeNods.includes(item) == true ? false :true} onChange={() => {
-                                    if(exeNods.includes(item)){
-                                        setexeNods([...exeNods.filter((ite) => ite!=item)])
-                                    }else{
-                                        setexeNods([...exeNods,item])
-                                    }
-                                }} id={item} />
-                                <label className="text-xs ml-1" htmlFor={item}>{item}</label>
-                            </div>
+                            <>
+                                <div className="flex items-start mb-2 justify-start">
+                                    <input type="checkbox" value={item} checked={exeNods.includes(item) == true ? false :true} onChange={() => {
+                                        if(exeNods.includes(item)){
+                                            const child:Array<string> = AllCategories[item]
+                                            setexeNods([...exeNods.filter((ite) => ite!=item && !child.includes(ite))])
+                                        }else{
+                                            const child:Array<string> = AllCategories[item]
+                                            child.forEach((el) => {
+                                                exeNods.push(el)
+                                            })
+                                            setexeNods([...exeNods,item])
+                                        }
+                                    }} id={item} />
+                                    <label className="text-xs ml-1" htmlFor={item}>{item}</label>
+                                </div>
+                                {
+                                    AllCategories[item]?.map((item:string) => {
+                                        return (
+                                            <div className="flex items-start ml-8 mb-2 justify-start">
+                                                <input type="checkbox" value={item} checked={exeNods.includes(item) == true ? false :true} onChange={() => {
+                                                    if(exeNods.includes(item)){
+                                                        setexeNods([...exeNods.filter((ite) => ite!=item)])
+                                                    }else{
+                                                        setexeNods([...exeNods,item])
+                                                    }
+                                                }} id={item} />
+                                                <label className="text-xs ml-1" htmlFor={item}>{item}</label>
+                                            </div>
+                                        )
+                                    })
+                                }
+                            </>
                         )
                     })}
                 </div>
