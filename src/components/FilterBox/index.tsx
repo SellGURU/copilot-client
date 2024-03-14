14eae492804fa73e29c8862be57f1b8fadf294cc
amir <amirdehghani137722@gmail.com>
2024-03-14 15:40:23 +0330

    add form controll

--- src/components/FilterBox/index.tsx
+++ src/components/FilterBox/index.tsx
@@ -2,7 +2,8 @@
 import React, { useState } from "react"
 import Nodes from "../../api/Nodes"
 import { useConstructor } from "../../help"
-
+import Checkbox from '@mui/material/Checkbox';
+import FormControlLabel from '@mui/material/FormControlLabel';
 interface FilterBox {
     exeNods:Array<string>
     setexeNods:(nods:Array<string>) =>void
@@ -22,8 +23,8 @@ const FilterBox:React.FC<FilterBox> = ({exeNods,setexeNods}) => {
     })
     return (
         <>
-            <div className="w-[250px] h-[400px] overflow-y-scroll chatBoxScroolBar bg-white shadow-md">
-                <div className="p-4 ">
+            <div className="w-[300px] resize h-[400px] overflow-y-scroll chatBoxScroolBar bg-white shadow-md">
+                {/* <div className="p-4 ">
                     <h5 className="">Categories</h5>
                     <hr className="my-2" />
                     <div className="flex items-start mb-2 justify-start">
@@ -80,6 +81,86 @@ const FilterBox:React.FC<FilterBox> = ({exeNods,setexeNods}) => {
                                         )
                                     })
                                 }
+                            </>
+                        )
+                    })}
+                </div> */}
+                <div className="p-5">
+                    <FormControlLabel
+                        label="All"
+                        sx={{ '& .MuiFormControlLabel-label': { fontSize: 14 } }}
+                        control={
+                        <Checkbox
+                            size="small"
+                            checked={checkAll}
+                            // indeterminate={checked[0] !== checked[1]}
+                            onChange={() => {
+                            setCheckAll(!checkAll)
+                            if(checkAll){
+                                const allData:Array<string> =[...categories]
+                                categories.forEach((el) => {
+                                    const child:Array<string> = AllCategories[el]
+                                    child?.forEach((el2) => {
+                                        if(!exeNods.includes(el2)){
+                                            allData.push(el2)
+                                        }
+                                    })
+                                })
+                                setexeNods(allData)
+                            }else{
+                                setexeNods([])
+                            }
+                        }} 
+                        />
+                        }
+                    />      
+                    <hr />              
+                    {categories.map((item:string) => {
+                        return (
+                            <>
+                                <FormControlLabel
+                                    sx={{ '& .MuiFormControlLabel-label': { fontSize: 13 } ,'& .MuiFormControlLabel-root':{margin: '8px 0px'}}}
+                                    label={item}
+                                    control={<Checkbox size={"small"} checked={exeNods.includes(item) == true ? false :true} onChange={() => {
+                                        if(exeNods.includes(item)){
+                                            const child:Array<string> = AllCategories[item]
+                                            setexeNods([...exeNods.filter((ite) => ite!=item && !child.includes(ite))])
+                                        }else{
+                                            const child:Array<string> = AllCategories[item]
+                                            child.forEach((el) => {
+                                                exeNods.push(el)
+                                            })
+                                            setexeNods([...exeNods,item])
+                                        }
+                                    }} />}
+                                />                      
+
+                                <div className="ml-4">
+                                    {AllCategories[item]?.map((item:string) => {
+                                        return (
+                                            <>
+                                            <FormControlLabel
+                                                label={item}
+                                                sx={{ '& .MuiFormControlLabel-label': { fontSize: 13 } }}
+                                                control={
+                                                    <Checkbox
+                                                        size="small"
+                                                        checked={exeNods.includes(item) == true ? false :true}
+                                                        onChange={() => {
+                                                            if(exeNods.includes(item)){
+                                                                setexeNods([...exeNods.filter((ite) => ite!=item)])
+                                                            }else{
+                                                                setexeNods([...exeNods,item])
+                                                            }
+                                                        }}                                                        
+                                                        // indeterminate={checked[0] !== checked[1]} 
+                                                    />
+                                                }
+                                            />                                             
+                                            </>
+                                        )
+                                    })}                                    
+                                </div>      
                             </>
                         )
                     })}
